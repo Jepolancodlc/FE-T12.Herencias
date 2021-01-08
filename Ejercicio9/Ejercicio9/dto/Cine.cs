d6104679d31cfee28d376cdeac7b9236428c3e6b
Jepolancodlc@gmail.com <Jepolancodlc@gmail.com>
2021-01-08 23:20:53 +0100

    #2

---
+++ Ejercicio9/Ejercicio9/dto/Cine.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio9
+{
+    class Cine 
+    {
+        //Atributos
+        private Peliculas pelicula;
+        private double precio = 0;
+
+        //Construsctores
+        public Cine()
+        {
+        }
+
+        public Cine(Peliculas pelicula, double precio)
+        {
+            this.Pelicula = pelicula;
+            this.precio = generarPrecio();
+        }
+
+
+        //Getters&Setters
+
+        public double Precio { get => precio; set => precio = value; }
+        public Peliculas Pelicula { get => pelicula; set => pelicula = value; }
+
+        //tString
+        public override string ToString()
+        {
+            return String.Format(" Pelicula---------\n {0} | Precio {1}$ |", Pelicula, precio);
+        }
+
+        public double generarPrecio()
+        {
+            Random random = new Random();
+            return Math.Round(random.NextDouble() * (0.3 - 0.2) + 0, 2);
+
+        }
+
+    }
+}
---
+++ Ejercicio9/Ejercicio9/dto/Espectador.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio9
+{
+    class Espectador
+
+    {
+        //Atributos
+        private string nombre="";
+        private int edad=0;
+        private double dinero=0;
+
+        //Constructores
+        public Espectador() //Constructor por defecto
+        {
+        }
+
+        public Espectador(string nombre, int edad, double dinero)
+        {
+            this.Nombre = generaNombre();
+            this.Edad = generarEdad();
+            this.Dinero = generarDinero();
+        }
+
+        //Getters&SetterS
+        public string Nombre { get => nombre; set => nombre = value; }
+        public int Edad { get => edad; set => edad = value; }
+        public double Dinero { get => dinero; set => dinero = value; }
+
+        //Tstring
+        public override string ToString()
+        {
+            return String.Format("\n Espectador----------- \n | Nombre: {0} | Edad: {1} años | Dinero {2}$|",nombre, edad,dinero);
+        }
+
+        //Metodos
+                                                                                      //Arrays que contienen los nombres y apellidos a seleccionar
+        string[] nombres = { "Juan", "Pedro", "Francisco", "Mia", "Alberto", "Maria", "Marco", "Teresa", "Federico", "Anais", "Andrea", "Isabel", "Emir", "Nate",
+        "Juana","Elena","Carla","Carlos","Leon","Elisabeth","Lana","Camila","Camilo","Esteban","Alejandra","Raquel","Blanca","Manuel","Marta","Diego","John","Dimitri"};
+        
+        string[] apellido = { "Ramirez","De La Cruz","Polanco","Rodriguez","Juarez","Kafter","Messi","De La Vega","Duran","Dubinov","Ruiz","Petreski","Kalashnikov","Drake",
+            "Falcon","Fragata","Chico","Bravo","Sanchez","Fernandez","Perez","Lopez","Alvarez","Flores","Smith","Johnson","Thompson","Makarov"};
+
+        public string generaNombre() //Genera nombre y apellido al espectador
+        {
+            Random random = new Random();
+     
+            int t = nombres.Length, o = apellido.Length;
+            string n = nombres[random.Next(t)]+" "+apellido[random.Next(o)] ;
+            return n;
+        }
+
+        public double generarDinero() //Genera la cantidad de dinero del espectador
+        {
+            Random random = new Random();
+            return Math.Round(random.NextDouble() * (50 - 0) + 0, 2);
+
+        }
+
+        public int generarEdad() //Genera la edad del espectador
+        {
+            Random random = new Random();
+            return random.Next(3, 99); 
+        }
+    }
+}
